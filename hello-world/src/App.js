15a4be8c1c563a54f9971c27cd64a74537f7a71c
sairidhima <sairidhima28012000@gmail.com>
2021-11-15 13:22:30 +0530

    first commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -5,15 +5,18 @@ import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
 import FRParentInput from './components/FRParentInput';
 import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 class App extends Component {
+  
   render() {
   return (
     <div className="App">
-      <PortalDemo />
-     {/* <FRParentInput /> */}
-     {/* <FocusInput /> */}
-      {/* <RefsDemo /> */}
+      <ClickCounter />
+      <HoverCounter />
     </div>
   )
 }
